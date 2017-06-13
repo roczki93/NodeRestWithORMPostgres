e5bc0b49d18f7195ec90c0581d73726700613a11
lukasz.rodak <lukasz.rodak@softiq.pl>
2017-06-13 21:27:10 +0200

    simple get implemented

---
+++ routes/index.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const router = express.Router();
+const pg = require('pg');
+const path = require('path');
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('postgres://myuser:mypass@192.168.177.183:5432/mydatabase1');
+
+var UserInformation = sequelize.define('user_information', {
+  firstName: {
+    type: Sequelize.STRING,
+    field: 'first_name' // Will result in an attribute that is firstName when user facing but first_name in the database
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    field: 'last_name'
+  },
+  email: {
+    type: Sequelize.STRING
+  },
+  gender: {
+    type: Sequelize.STRING
+  },
+  city: {
+    type: Sequelize.STRING
+  },
+  street: {
+    type: Sequelize.STRING
+  },
+  latitude: {
+    type: Sequelize.STRING
+  },
+  longitude: {
+    type: Sequelize.STRING
+  }
+}, {
+  timestamps: false,
+  freezeTableName: true // Model tableName will be the same as the model name
+});
+
+// UserInformation.sync({force: false}).then(function () {
+//   // Table created
+//   return UserInformation.create({
+//     firstName: 'Lukasz',
+//     lastName: 'Lukasimir',
+//     email: 'taki@email.com',
+//     gender: 'male',
+//     city: 'Gliwice',
+//     street: '22 Dluga',
+//     latitude: '40.0000',
+//     longitude: '50.9990'
+//   });
+// });
+//get user info
+router.get('/api/v1/userinfo', (req, res, next) => {
+    return UserInformation
+      .findAll({ 
+        order: [['id', 'DESC']], 
+        limit: 10 
+      })
+      .then(UserInformation => res.status(200).send(UserInformation));
+});
+router.get('/api/v1/userinfo/:findParam', (req, res, next) => {
+    return UserInformation
+      .find({ 
+        where: {
+          firstName: req.params.findParam 
+        }
+      })
+      .then(UserInformation => res.status(200).send(UserInformation));
+});
+//end get user info
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  UserInformation.findOne().then(function (userInfo) {
+    console.log(userInfo.get());
+  });
+  
+      UserInformation.findOne().then(function (userInfo) {
+          res.json(userInfo.get());
+      });
+    
+  // res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
